b388694e6bdd1d41d1a8cc4e40c8f2314b06712a
piyushjaipuriar <142023718+piyushjaipuriar@users.noreply.github.com>
2023-12-10 13:42:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+// Prompt user for the first input
+var firstInput = prompt("Enter the first number:");
+
+// Multiply the first input by 2
+var result = parseInt(firstInput) * 2;
+
+// Prompt user for the second input
+var secondInput = prompt("Enter the second number:");
+
+// Add the second input to the result
+result += parseInt(secondInput);
+
+// Display the final result using alert
+alert("The final result is: " + result);
+
